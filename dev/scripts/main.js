098ce2d45fba38d41c3d4fb4402a3a44a08cc201
erick <erick.marin@adesis.com>
2015-12-02 13:46:27 -0600

    fix(): new styles and functions

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,13 +1,23 @@
-$(document).ready(function() {
-    $('li').click(function() {
+$(document).ready( function() {
+    $('span').click( function() {
         var ids = $(this).attr('id');
         $('div[id="'+ids+'"').addClass('boxes');
-        $('ul').addClass('menu_move');
-        $('li').slideToggle('slow');
+        $('div[id="'+ids+'"').css('display', 'block');
+        $('div.menu').addClass('menu_move');
+        $('div.menu').slideUp('slow');
     });
     $('div[class="close"]').click( function() {
-        $('.cont-gallery').removeClass('boxes');
-        $('ul').removeClass('menu_move');
-        $('li').slideDown();
+        $('div.cont-gallery').slideUp('fast');
+        $('div.menu').removeClass('menu_move');
+        $('div.menu').slideDown('slow');
     });
+    function getRandomColor() {
+        $('span').each( function() {
+            var color = Math.floor(Math.random()*16777215).toString(16);
+            $(this).css('background-image', 'linear-gradient(#'+color+', #333)');
+            $(this).css('background-image', '-webkit-linear-gradient(#'+color+', #333)');
+            $(this).css('background-image', '-moz-linear-gradient(#'+color+', #333)');
+        });
+    }
+    getRandomColor();
 });
